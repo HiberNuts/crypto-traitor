3f31502466e45af56117422727e69ecf99403a84
raghav <raghavjindal0212@gmail.com>
2022-11-08 23:01:51 +0530

    desc changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,11 +9,8 @@ const Hero = ({ connectWallet, CurrentAccount, setCurrentAccount, noOfMint, setn
       <div class="container flex flex-row items-center px-6 mx-auto mt-10 mt-40 space-y-0 md:space-y-0 md:flex-row">
         {/* <!-- Left item --> */}
         <div class="flex flex-col mb-32 space-y-12 md:w-1/2">
-          <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">VIRES IN NUMERIS</h1>
-          <p class="max-w-sm text-center  md:text-left">
-            Let's empower individuals to find their potential in web3, connecting with our higher selves in the space
-            through professional development guided by thought leaders in Web 3 and the arts.
-          </p>
+          <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">Bored & Better Azurians</h1>
+          <p class="max-w-sm text-center  md:text-left">We got bored and created a better for arts for Azurbala.</p>
           <div class="flex justify-center md:justify-start">
             <Herobtn askContractToMintNft={askContractToMintNft} />
           </div>
